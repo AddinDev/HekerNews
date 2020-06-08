74fa8756f9fd7efd051218c44ad6438bd512fdef
AddinDev <54827832+AddinDev@users.noreply.github.com>
2020-06-08 16:11:19 +0700

    Add files via upload

---
+++ HekerNews/Models/NetworkManager.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkManager.swift
+//  HekerNews
+//
+//  Created by pasto on 08/06/20.
+//  Copyright © 2020 pstw._. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager: ObservableObject {
+    
+    @Published var posts = [Post]()
+    
+    func fetchData() {
+        if let url = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error == nil {
+                    let decoder = JSONDecoder()
+                    if let safeData = data {
+                        do {
+                            let results = try decoder.decode(Result.self, from: safeData)
+                            DispatchQueue.main.async {
+                                self.posts = results.hits
+                            }
+                        }
+                        catch {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}
+
